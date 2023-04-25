6e14e6e78869626bc398f945ac54a009cb5daf60
androosk <atirpok3@gmail.com>
2023-04-25 17:25:24 -0600

    placeholder tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import thirdAndrew from './assets/ThirdAndrewCreative_1.2.1.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,14 +10,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://thirdandrewcreative.com" target="_blank">
+          <img src={thirdAndrew} className="logo" alt="Third Andrew Logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Third Andrew Creative Agency</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
