7d21ae2eaaa3cb7afc47bdfe4e77ff850ee2d85d
androosk <atirpok3@gmail.com>
2023-04-27 10:34:23 -0600

    added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import thirdAndrew from './assets/ThirdAndrewCreative_1.2.1.png'
 
